befa684921585cb511734aa33c5bcfea0464c57b
JLee1210 <jadenjlee@outlook.com>
2020-04-22 16:27:53 -0400

    Added UI and some functionality logic

--- src/3d_sim.js
+++ src/3d_sim.js
@@ -2,41 +2,87 @@ import * as THREE from "../node_modules/three/build/three.module.js";
 import { PLYLoader } from "../node_modules/three/examples/jsm/loaders/PLYLoader.js";
 import { OrbitControls } from "../node_modules/three/examples/jsm/controls/OrbitControls.js";
 
-const canvas = document.querySelector("#grid");
-
 const fov = 60; // field of view
 const aspect = window.innerWidth / window.innerHeight; // display aspect (default: 300x150)
 const near = 1;
 const far = 1000;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
-const controls = new OrbitControls(camera, canvas);
+const objCanvas = document.querySelector("#objGrid");
+const objCamera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+const objControls = new OrbitControls(objCamera, objCanvas);
+const objScene = new THREE.Scene();
+const objRenderer = new THREE.WebGLRenderer({ canvas: objCanvas });
 
-const scene = new THREE.Scene();
+const sensorCanvas = document.querySelector("#sensorGrid");
+const sensorCamera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+const sensorControls = new OrbitControls(sensorCamera, sensorCanvas);
+const sensorScene = new THREE.Scene();
+const sensorRenderer = new THREE.WebGLRenderer({ canvas: sensorCanvas });
 
-const renderer = new THREE.WebGLRenderer({ canvas });
+const plyLoader = new PLYLoader();
 
-let horn;
+// let horn;
+// let servo;
+
+const objects = new Array(9);
 const animations = [];
+
+// Add animation when generating a function
 animations.push(false);
 animations.push(false);
 
-init();
+objMain();
+loadObject();
+sensorMain();
+
+sensorCanvas.style.display = "none";
 document.getElementById("anim1").onclick = function () {
-  document.getElementById("anim" + (1).toString()).disabled = true;
+  objects[0].forEach((val) => {
+    val.visible = true;
+  });
+  objects.forEach((val, index) => {
+    if (index !== 0 && val != undefined) {
+      val.forEach((obj) => {
+        obj.visible = false;
+      });
+    }
+  });
   animations[0] = true;
   animations.forEach((val, index) => {
-    if (index !== 0) {
+    if (index !== 0 && val != undefined) {
       animations[index] = false;
       document.getElementById("anim" + (index + 1).toString()).disabled = false;
     }
   });
-  horn.rotation.y = 0;
-  animateClockwise((90 * Math.PI) / 180, 0);
+  document.getElementById("play").onclick = function () {
+    if (isReset) {
+      animations[0] = true;
+      objects[0][1].rotation.y = 0;
+    }
+    changePlayButton();
+    animateClockwise((90 * Math.PI) / 180, 0, objects[0]);
+  };
 };
 
+$("#sensors").on("show.bs.collapse", function () {
+  sensorCanvas.style.display = "initial";
+});
+
+$("#sensors").on("hide.bs.collapse", function () {
+  sensorCanvas.style.display = "none";
+});
+
 document.getElementById("anim2").onclick = function () {
-  document.getElementById("anim" + (2).toString()).disabled = true;
+  objects[1].forEach((val) => {
+    val.visible = true;
+  });
+  objects.forEach((val, index) => {
+    if (index !== 1 && val != undefined) {
+      val.forEach((obj) => {
+        obj.visible = false;
+      });
+    }
+  });
   animations[1] = true;
   animations.forEach((val, index) => {
     if (index !== 1) {
@@ -44,59 +90,122 @@ document.getElementById("anim2").onclick = function () {
       document.getElementById("anim" + (index + 1).toString()).disabled = false;
     }
   });
-  horn.rotation.y = 0;
-  animateCounterClockwise((90 * Math.PI) / 180, 1);
+  document.getElementById("play").onclick = function () {
+    changePlayButton();
+    objects[1][1].rotation.y = 0;
+    animateCounterClockwise((90 * Math.PI) / 180, 1);
+  };
 };
 
-function init() {
-  camera.position.z = 50;
-  if (resizeRendererToDisplaySize(renderer)) {
-    const canvas = renderer.domElement;
-    camera.aspect = canvas.clientWidth / canvas.clientHeight;
-    camera.updateProjectionMatrix();
+let isPlay = false;
+let isReset = false;
+function changePlayButton() {
+  const playID = document.getElementById("play");
+  if (!isPlay && !isReset) {
+    playID.innerHTML = "Pause";
+    isPlay = true;
+  } else if (isPlay && !isReset) {
+    playID.innerHTML = "Resume";
+    isPlay = false;
+  } else {
+    playID.innerHTML = "Play";
+    isReset = false;
+    isPlay = false;
+  }
+}
+
+function objMain() {
+  objCamera.position.z = 50;
+  if (resizeRendererToDisplaySize(objRenderer)) {
+    const canvas = objRenderer.domElement;
+    objCamera.aspect = canvas.clientWidth / canvas.clientHeight;
+    objCamera.updateProjectionMatrix();
   }
 
-  controls.target.set(0, 5, 0);
-  controls.update();
+  objControls.target.set(0, 5, 0);
+  objControls.update();
 
-  scene.background = new THREE.Color(0xcccccc);
-  scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
+  objScene.background = new THREE.Color(0xcccccc);
+  objScene.fog = new THREE.FogExp2(0xcccccc, 0.002);
 
   /*
-   * ------------- LIGHTING -----------------
+   * ------------- LIGHTING ------------
    */
-  // {
-  //   const skyColor = 0xb1e1ff; // light blue
-  //   const groundColor = 0xb97a20; // brownish orange
-  //   const intensity = 1;
-  //   const hlight = new THREE.HemisphereLight(skyColor, groundColor, intensity);
-  //   scene.add(hlight);
-  // }
+  const light_d1 = new THREE.DirectionalLight(0xffffff);
+  light_d1.position.set(1, 1, 1);
+  objScene.add(light_d1);
 
-  // {
-  //   const color = 0xffffff;
-  //   const intensity = 1;
-  //   const dlight = new THREE.DirectionalLight(color, intensity);
-  //   dlight.position.set(0, 10, 0);
-  //   dlight.target.position.set(-5, 0, 0);
-  //   scene.add(dlight);
-  //   scene.add(dlight.target);
-  // }
+  const light_d2 = new THREE.DirectionalLight(0x002288);
+  light_d2.position.set(-1, -1, -1);
+  objScene.add(light_d2);
+
+  const light_a = new THREE.AmbientLight(0x222222);
+  objScene.add(light_a);
   /*
-   * ------------- LIGHTING -----------------
+   * ------------- LIGHTING ------------
    */
+  window.addEventListener(
+    "resize",
+    onWindowResize(objRenderer, objCamera, objScene),
+    false
+  );
+  objRender();
+}
 
-  const plyLoader = new PLYLoader();
+function sensorMain() {
+  sensorCamera.position.z = 50;
+  if (resizeRendererToDisplaySize(sensorRenderer)) {
+    const canvas = sensorRenderer.domElement;
+    sensorCamera.aspect = canvas.clientWidth / canvas.clientHeight;
+    sensorCamera.updateProjectionMatrix();
+  }
+
+  sensorControls.target.set(0, 5, 0);
+  sensorControls.update();
+
+  sensorScene.background = new THREE.Color(0xcccccc);
+  sensorScene.fog = new THREE.FogExp2(0xcccccc, 0.002);
+
+  /*
+   * ------------- LIGHTING ------------
+   */
+  const light_d1 = new THREE.DirectionalLight(0xffffff);
+  light_d1.position.set(1, 1, 1);
+  sensorScene.add(light_d1);
+
+  const light_d2 = new THREE.DirectionalLight(0x002288);
+  light_d2.position.set(-1, -1, -1);
+  sensorScene.add(light_d2);
+
+  const light_a = new THREE.AmbientLight(0x222222);
+  sensorScene.add(light_a);
+  /*
+   * ------------- LIGHTING ------------
+   */
+  window.addEventListener(
+    "resize",
+    onWindowResize(sensorRenderer, sensorCamera, sensorScene),
+    false
+  );
+  sensorRender();
+}
+
+function loadObject() {
+  objects[0] = [];
+  objects[1] = [];
   plyLoader.load("../ply/servo.ply", (ply) => {
     ply.computeVertexNormals();
     const material = new THREE.MeshStandardMaterial({
       color: 0x0055ff,
       flatShading: true,
     });
-    const mesh = new THREE.Mesh(ply, material);
-    mesh.castShadow = true;
-    mesh.receiveShadow = true;
-    scene.add(mesh);
+    let servo = new THREE.Mesh(ply, material);
+    servo.visible = false;
+    servo.castShadow = true;
+    servo.receiveShadow = true;
+    objects[0].push(servo);
+    //objects[1].push(servo);
+    objScene.add(servo);
   });
 
   plyLoader.load("../ply/horn.ply", (ply) => {
@@ -106,8 +215,8 @@ function init() {
       color: 0xffffff,
       flatShading: true,
     });
-    horn = new THREE.Mesh(ply, material);
-
+    let horn = new THREE.Mesh(ply, material);
+    horn.visible = false;
     horn.position.x = -5;
     horn.position.y = -(-15);
     horn.rotation.z = Math.PI;
@@ -116,29 +225,10 @@ function init() {
 
     horn.castShadow = true;
     horn.receiveShadow = true;
-
-    scene.add(horn);
+    objects[0].push(horn);
+    //objects[1].push(horn);
+    objScene.add(horn);
   });
-
-  /*
-   * ------------- LIGHTING ------------
-   */
-  const light_d1 = new THREE.DirectionalLight(0xffffff);
-  light_d1.position.set(1, 1, 1);
-  scene.add(light_d1);
-
-  const light_d2 = new THREE.DirectionalLight(0x002288);
-  light_d2.position.set(-1, -1, -1);
-  scene.add(light_d2);
-
-  const light_a = new THREE.AmbientLight(0x222222);
-  scene.add(light_a);
-  /*
-   * ------------- LIGHTING ------------
-   */
-  window.addEventListener("resize", onWindowResize, false);
-  //animateCounterClockwise((90 * Math.PI) / 180);
-  render();
 }
 
 function resizeRendererToDisplaySize(renderer) {
@@ -153,7 +243,7 @@ function resizeRendererToDisplaySize(renderer) {
   return needResize;
 }
 
-function onWindowResize() {
+function onWindowResize(renderer, camera, scene) {
   if (resizeRendererToDisplaySize(renderer)) {
     const canvas = renderer.domElement;
     camera.aspect = canvas.clientWidth / canvas.clientHeight;
@@ -162,39 +252,44 @@ function onWindowResize() {
   renderer.render(scene, camera);
 }
 
-function animateClockwise(targetRadian, ftnIndex) {
-  if (horn.rotation.y >= -targetRadian) {
-    horn.rotation.y -= 0.01;
+//****************** ANIMATIONS *******************//
+
+function animateClockwise(targetRadian, ftnIndex, object) {
+  if (object[1].rotation.y >= -targetRadian) {
+    object[1].rotation.y -= 0.01;
   } else {
     animations[ftnIndex] = false;
-    document.getElementById(
-      "anim" + (ftnIndex + 1).toString()
-    ).disabled = false;
+    isReset = true;
+    document.getElementById("play").innerHTML = "Reset";
   }
-  if (animations[ftnIndex]) {
+  if (animations[ftnIndex] && isPlay) {
     requestAnimationFrame(() => {
-      animateClockwise(targetRadian, ftnIndex);
+      animateClockwise(targetRadian, ftnIndex, object);
     });
   }
 }
 
-function animateCounterClockwise(targetRadian, ftnIndex) {
-  if (horn.rotation.y <= targetRadian) {
-    horn.rotation.y += 0.01;
+function animateCounterClockwise(targetRadian, ftnIndex, object) {
+  if (object[1].rotation.y <= targetRadian) {
+    object[1].rotation.y += 0.01;
   } else {
     animations[ftnIndex] = false;
-    document.getElementById(
-      "anim" + (ftnIndex + 1).toString()
-    ).disabled = false;
+    isReset = true;
+    document.getElementById("play").innerHTML = "Reset";
   }
-  if (animations[ftnIndex]) {
+  if (animations[ftnIndex] && isPlay) {
     requestAnimationFrame(() => {
       animateCounterClockwise(targetRadian, ftnIndex);
     });
   }
 }
 
-function render() {
-  renderer.render(scene, camera);
-  requestAnimationFrame(render);
+function objRender() {
+  objRenderer.render(objScene, objCamera);
+  requestAnimationFrame(objRender);
+}
+
+function sensorRender() {
+  sensorRenderer.render(sensorScene, sensorCamera);
+  requestAnimationFrame(sensorRender);
 }
