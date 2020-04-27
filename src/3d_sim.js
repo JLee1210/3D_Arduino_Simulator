e82778c5e6a1865f544227526db21c5344fac2cb
JLee1210 <jadenjlee@outlook.com>
2020-04-27 02:45:32 -0400

    Added and aligned motor

--- src/3d_sim.js
+++ src/3d_sim.js
@@ -2,9 +2,9 @@ import * as THREE from "../node_modules/three/build/three.module.js";
 import { PLYLoader } from "../node_modules/three/examples/jsm/loaders/PLYLoader.js";
 import { OrbitControls } from "../node_modules/three/examples/jsm/controls/OrbitControls.js";
 
-const fov = 60; // field of view
+const fov = 100; // field of view
 const aspect = window.innerWidth / window.innerHeight; // display aspect (default: 300x150)
-const near = 1;
+const near = 0.1;
 const far = 1000;
 
 const objCanvas = document.querySelector("#objGrid");
@@ -211,29 +211,18 @@ function sensorMain() {
 function loadObject() {
   objects[0] = [];
   objects[1] = [];
-  plyLoader.load("../ply/servo.ply", (ply) => {
-    ply.computeVertexNormals();
-    const material = new THREE.MeshStandardMaterial({
-      color: 0x0055ff,
-      flatShading: true,
-    });
-    let servo = new THREE.Mesh(ply, material);
-    servo.visible = false;
-    servo.castShadow = true;
-    servo.receiveShadow = true;
-    objects[0][1] = servo;
-    //objects[1].push(servo);
-    objScene.add(servo);
-  });
-
   plyLoader.load("../ply/horn.ply", (ply) => {
     ply.computeVertexNormals();
-
     const material = new THREE.MeshStandardMaterial({
-      color: 0xffffff,
+      color: 0xffffff, // white
       flatShading: true,
     });
     let horn = new THREE.Mesh(ply, material);
+    horn.traverse(function (child) {
+      if (child instanceof THREE.Mesh) {
+        child.geometry.center();
+      }
+    });
     horn.visible = false;
     horn.position.x = -5;
     horn.position.y = -(-15);
@@ -245,6 +234,136 @@ function loadObject() {
     //objects[1].push(horn);
     objScene.add(horn);
   });
+
+  plyLoader.load("../ply/servo.ply", (ply) => {
+    ply.computeVertexNormals();
+    const material = new THREE.MeshStandardMaterial({
+      color: 0x0055ff,
+      flatShading: true,
+    });
+    let servo = new THREE.Mesh(ply, material);
+    servo.traverse(function (child) {
+      if (child instanceof THREE.Mesh) {
+        child.geometry.center();
+      }
+    });
+    servo.visible = false;
+    servo.castShadow = true;
+    servo.receiveShadow = true;
+    objects[0][1] = servo;
+    //objects[1].push(servo);
+    objScene.add(servo);
+    resizeCameraControls(servo);
+  });
+
+  plyLoader.load("../ply/Wheel.ply", (ply) => {
+    ply.computeVertexNormals();
+    const material = new THREE.MeshStandardMaterial({
+      color: 0x696969, // white
+      flatShading: true,
+    });
+    let wheel = new THREE.Mesh(ply, material);
+    wheel.traverse(function (child) {
+      if (child instanceof THREE.Mesh) {
+        child.geometry.center();
+      }
+    });
+    wheel.visible = true;
+    wheel.position.x = -10;
+    wheel.position.y = -3;
+    wheel.position.z = 10;
+    wheel.rotation.z = Math.PI;
+    wheel.scale.y = 0.3;
+    wheel.scale.x = 0.3;
+    wheel.scale.z = 0.3;
+
+    wheel.castShadow = true;
+    wheel.receiveShadow = true;
+    objects[1][0] = wheel;
+    objScene.add(wheel);
+    //resizeCameraControls(wheel);
+  });
+
+  plyLoader.load("../ply/Wheel.ply", (ply) => {
+    ply.computeVertexNormals();
+    const material = new THREE.MeshStandardMaterial({
+      color: 0x696969, // white
+      flatShading: true,
+    });
+    let wheel = new THREE.Mesh(ply, material);
+    wheel.traverse(function (child) {
+      if (child instanceof THREE.Mesh) {
+        child.geometry.center();
+      }
+    });
+    wheel.visible = true;
+    wheel.position.x = 10;
+    wheel.position.y = -3;
+    wheel.position.z = 10;
+    wheel.rotation.z = 0;
+    wheel.scale.y = 0.3;
+    wheel.scale.x = 0.3;
+    wheel.scale.z = 0.3;
+
+    wheel.castShadow = true;
+    wheel.receiveShadow = true;
+    objects[1][1] = wheel;
+    objScene.add(wheel);
+    //resizeCameraControls(wheel);
+  });
+  plyLoader.load("../ply/DC_motor_2.ply", (ply) => {
+    ply.computeVertexNormals();
+    const material = new THREE.MeshStandardMaterial({
+      color: 0x0055ff, // blue
+      flatShading: true,
+    });
+
+    let motor = new THREE.Mesh(ply, material);
+    motor.traverse(function (child) {
+      if (child instanceof THREE.Mesh) {
+        child.geometry.center();
+      }
+    });
+    motor.visible = true;
+    motor.position.x = 0;
+    motor.position.y = 0;
+    motor.position.z = 0;
+    motor.scale.y = 0.5;
+    motor.scale.z = 0.5;
+    motor.scale.x = 0.5;
+
+    motor.rotation.x = Math.PI;
+    //motor.rotation.z = Math.PI;
+
+    motor.castShadow = true;
+    motor.receiveShadow = true;
+    objects[1][2] = motor;
+    objScene.add(motor);
+    resizeCameraControls(motor);
+  });
+}
+
+const boundingBox = new THREE.Box3();
+const offset = 10 || 1.25;
+
+function resizeCameraControls(obj) {
+  boundingBox.setFromObject(obj);
+  const center = boundingBox.getCenter();
+  const size = boundingBox.getSize();
+  const maxDim = Math.max(size.x, size.y, size.z);
+  const fov = objCamera.fov * (Math.PI / 180);
+  let cameraZ = Math.abs((maxDim / 3.5) * Math.tan(fov * 2));
+  cameraZ *= offset;
+  const minZ = boundingBox.min.z;
+  const cameraToFarEdge = minZ < 0 ? -minZ + cameraZ : cameraZ - minZ;
+  objCamera.position.z = center.z + cameraZ;
+
+  objCamera.lookAt(center);
+  objControls.target = center;
+
+  // prevent camera from zooming out far enough to create far plane cutoff
+  objControls.maxDistance = cameraToFarEdge * 4;
+  objControls.saveState();
 }
 
 function resizeRendererToDisplaySize(renderer) {
